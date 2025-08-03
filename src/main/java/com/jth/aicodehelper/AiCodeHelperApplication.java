246dc4f1e45b0d9ef513394f41d75954617d1b8f
ymss <1341424760@qq.com>
2025-08-03 17:21:44 +0800

    test

--- src/main/java/com/jth/aicodehelper/AiCodeHelperApplication.java
+++ src/main/java/com/jth/aicodehelper/AiCodeHelperApplication.java
@@ -8,7 +8,6 @@ public class AiCodeHelperApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AiCodeHelperApplication.class, args);
-        System.out.println("hello");
     }
 
 }
