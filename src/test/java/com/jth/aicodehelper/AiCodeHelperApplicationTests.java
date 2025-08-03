f777ad3b15f163613df127e61878807383915a6c
ymss <1341424760@qq.com>
2025-08-03 16:04:30 +0800

    test

---
+++ src/test/java/com/jth/aicodehelper/AiCodeHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jth.aicodehelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
